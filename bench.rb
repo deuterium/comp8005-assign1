71d6a6b186f66069b31a9177c6139ae9f50bbc93
Chris Wood <chriswood.ca@gmail.com>
2014-01-26 18:13:08 -0800

    Completed basic threading
    5 threads concurrently execute now. Need some sort of heavy processing
    to run....
    Added logging to file bench_log (ignored from git)

--- bench.rb
+++ bench.rb
@@ -4,6 +4,7 @@
 #Use multiple processes and threads on either the Windows or Linux operating systems and measure the performance and efficiency of each mechanism.
 
 require 'prime'
+require 'thread'
 
 #global variables
 usage = "Proper usage: ./bench [t|p]"
@@ -11,22 +12,55 @@ usage = "Proper usage: ./bench [t|p]"
 
 
 #functions
+def timeNow
+	return "#{Time.now}"
+end
+
+def info(msg)
+	File.open("bench_log", 'a') { |f| f.write ("#{msg} \n") }
+	puts msg
+end
+
 def benchProcesses
-	puts "#{@num_workers} processes"
+	info "#{@num_workers} processes"
+	pids = Array.new(@num_workers)
+	for i in 0..@num_workers
+		pids[i] = i
+	end
+	puts pids
 end
 
 def benchThreads
-	puts "#{@num_workers} threads"
-	doWork
+	info "Starting thread benchmarking at #{timeNow}"
+	threads = Array.new(@num_workers)
+	info "Starting total of #{@num_workers} threads at #{timeNow}"
+	for i in 0..@num_workers
+		threads[i] = Thread.new{ doWork(i,55) }
+	end
+	#wait for each thread to finish before continuing
+	for i in 0..@num_workers
+		threads[i].join
+	end
+	
+	puts "Completed thread benchmarking at #{timeNow}"
 end
 
-def doWork
-	pr = Integer(STDIN.gets)
-	puts Prime.prime_division(pr)
+#55 = thread, 33 = process
+def doWork(id, type)
+	if type == 55
+		info "#{Thread.current} starting at #{timeNow}"
+		#Prime.prime_division(22)
+		sleep(rand(3..10))
+		info "#{Thread.current} end at #{timeNow}"
+	elsif type == 33
+
+	else
+		info "should get here!!!"
+		exit
+	end	
 end
 
-
-#program logic
+#program main
 if ARGV.count > 1 || ARGV.empty?
 	puts usage
 	exit
