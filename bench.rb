c986cb31d32bd0dd1833d2a3d0e0225096893042
Chris Wood <chriswood.ca@gmail.com>
2014-01-26 22:19:41 -0800

    Removed unneeded require
    No longer using mutexes so thread not needed

--- bench.rb
+++ bench.rb
@@ -4,7 +4,6 @@
 #Use multiple processes and threads on either the Windows or Linux operating systems and measure the performance and efficiency of each mechanism.
 
 require 'prime'
-require 'thread'
 
 ##global variables
 @num_workers = 5
